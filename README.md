0bd998c6a8a13d733918ec6f1c9f797ea2e4c59e
Ruben Bezuidenhout <ruben@bitcube.tech>
2019-11-21 18:46:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Electron Template
+
+Small electron template with React, Electron, TypeScript and Babel
+
+## Pre requisites
+
+- Node v13.1.0
+- Yarn 1.19.1
+
+## Getting Started
+
+Make sure you have the pre requisites installed
+
+### Quick start (aka Copy-Past-Run)
+
+```bash
+git clone https://github.com/RubenBez/electron-template.git
+cd electron-template
+yarn
+yarn run start
+```
+
+### Step-by-Step
+
+1. Clone the repository `git clone https://github.com/RubenBez/electron-template.git`
+2. Open the directory `cd electron-template`
+3. Install the dependencies with `yarn`
+4. Start the app with `yarn run start`
---
+++ webpack.rules.js
@@ -0,0 +1,40 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: "node-loader"
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: "@marshallofsound/webpack-asset-relocator-loader",
+      options: {
+        outputAssetBase: "native_modules"
+      }
+    }
+  },
+  {
+    // Include ts, tsx, js, and jsx files.
+    test: /\.(ts|js)x?$/,
+    exclude: /(node_modules|.webpack)/,
+    loader: "babel-loader"
+  }
+
+  // Put your webpack loader rules in this array.  This is where you would put
+  // your ts-loader configuration for instance:
+  /**
+   * Typescript Example:
+   *
+   * {
+   *   test: /\.tsx?$/,
+   *   exclude: /(node_modules|.webpack)/,
+   *   loaders: [{
+   *     loader: 'ts-loader',
+   *     options: {
+   *       transpileOnly: true
+   *     }
+   *   }]
+   * }
+   */
+];
